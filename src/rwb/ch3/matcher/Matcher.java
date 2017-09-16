e43abc97c0cedb3c7244b9d956caf0f79392031e
adyang <adyang2011@gmail.com>
2017-09-16 20:59:50 +0800

    Refactor matcher to make comments redundant and refactor test

--- src/rwb/ch3/matcher/Matcher.java
+++ src/rwb/ch3/matcher/Matcher.java
@@ -1,24 +1,30 @@
 package rwb.ch3.matcher;
 
 public class Matcher {
-	public Matcher() {
+	public boolean match(int[] expected, int[] actual, int clipLimit, int delta) {
+		clipValuesOverLimit(actual, clipLimit);
+		return isSameLength(expected, actual) && isWithinDelta(expected, actual, delta);
 	}
 
-	public boolean match(int[] expected, int[] actual, int clipLimit, int delta) {
-		// Clip "too-large" values
+	private void clipValuesOverLimit(int[] actual, int clipLimit) {
 		for (int i = 0; i < actual.length; i++)
 			if (actual[i] > clipLimit)
 				actual[i] = clipLimit;
+	}
 
-		// Check for length differences
-		if (actual.length != expected.length)
-			return false;
+	private boolean isSameLength(int[] expected, int[] actual) {
+		return actual.length == expected.length;
+	}
 
-		// Check that each entry within expected +/- delta
-		for (int i = 0; i < actual.length; i++)
-			if (Math.abs(expected[i] - actual[i]) > delta)
+	private boolean isWithinDelta(int[] expected, int[] actual, int delta) {
+		for (int i = 0; i < actual.length; i++) {
+			if (entryIsNotWithinDelta(expected[i], actual[i], delta))
 				return false;
-
+		}
 		return true;
+	}
+
+	private boolean entryIsNotWithinDelta(int expectedEntry, int actualEntry, int delta) {
+		return Math.abs(expectedEntry - actualEntry) > delta;
 	}
 }
--- src/rwb/ch3/matcher/MatcherTest.java
+++ src/rwb/ch3/matcher/MatcherTest.java
@@ -1,25 +1,38 @@
 package rwb.ch3.matcher;
 
-import org.junit.Test;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class MatcherTest {
+	private static final int[] EXPECTED = new int[] { 10, 50, 30, 98 };
+	private static final int CLIP_LIMIT = 100;
+	private static final int DELTA = 5;
+
+	private Matcher matcher;
+
+	@Before
+	public void setUp() {
+		matcher = new Matcher();
+	}
+
 	@Test
-	public void testMatch() {
-        Matcher matcher = new Matcher();
+	public void testMatch_WhenSameLengthAndWithinDelta() {
+		int[] actual = new int[] { 12, 55, 25, 110 };
+		assertTrue(matcher.match(EXPECTED, actual, CLIP_LIMIT, DELTA));
+	}
 
-        int[] expected = new int[] {10, 50, 30, 98};
-        int clipLimit = 100;
-        int delta = 5;
+	@Test
+	public void testMatch_WhenSameLengthButExceedDelta() {
+		int[] actual = new int[] { 10, 60, 30, 98 };
+		assertFalse(matcher.match(EXPECTED, actual, CLIP_LIMIT, DELTA));
+	}
 
-        int[] actual = new int[] {12, 55, 25, 110};
-
-        assertTrue(matcher.match(expected, actual, clipLimit, delta));
-
-        actual = new int[] {10, 60, 30, 98};
-        assertTrue(!matcher.match(expected, actual, clipLimit, delta));
-
-        actual = new int[] {10, 50, 30};
-        assertTrue(!matcher.match(expected, actual, clipLimit, delta));
-    }
+	@Test
+	public void testMatch_WhenDifferentLength() {
+		int[] actual = new int[] { 10, 50, 30 };
+		assertFalse(matcher.match(EXPECTED, actual, CLIP_LIMIT, DELTA));
+	}
 }
